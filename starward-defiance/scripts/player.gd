953d8716176f6dcb13d7d081d33855626970316d
Kason <kason.suchow@gmail.com>
2025-02-21 13:55:17 -0700

    running, jumping and falling animations

--- starward-defiance/scripts/player.gd
+++ starward-defiance/scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,9 +15,27 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walking")
+	else:
+		if velocity.y > 0:
+			animated_sprite.play("falling")
+		else:
+			animated_sprite.play("jumping")
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
