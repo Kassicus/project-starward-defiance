b391a2fd9d596c1c6c3993afbaed04acbdc95bf2
Kason <kason.suchow@gmail.com>
2025-02-21 19:56:36 -0700

    not sure about having a slowdown effect
    Currently [SHIFT] activates bullet time as long as its held down. Kinda nice for hitting precise wall jumps...

--- starward-defiance/scripts/player.gd
+++ starward-defiance/scripts/player.gd
@@ -12,6 +12,11 @@ const JUMP_VELOCITY = -400.0
 var current_jumps = 0
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("bullet_time"):
+		Engine.time_scale = 0.2
+	else:
+		Engine.time_scale = 1
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
